d81e37423f958e62b4b0c880ebeff639beaa8347
fdupont-redhat <fdupont@redhat.com>
2018-12-03 09:27:48 +0100

    Move security groups association to network port

--- files/virt-v2v-wrapper.py
+++ files/virt-v2v-wrapper.py
@@ -255,6 +255,8 @@ class OSPHost(BaseHost):
                 port_cmd.extend([
                     '--fixed-ip', 'ip-address=%s' % nic['ip_address'],
                     ])
+            for grp in data['osp_security_groups_ids']:
+                port_cmd.extend(['--security-group', grp])
             try:
                 port = self._run_openstack(port_cmd, data, destination=True)
             except subprocess.CalledProcessError as e:
@@ -270,8 +272,6 @@ class OSPHost(BaseHost):
             'server', 'create',
             '--flavor', data['osp_flavor_id'],
             ]
-        for grp in data['osp_security_groups_ids']:
-            os_command.extend(['--security-group', grp])
         os_command.extend(['--volume', volumes[0]])
         for i in xrange(1, len(volumes)):
             os_command.extend([
@@ -727,7 +727,7 @@ class VDSMHost(BaseHost):
 
             if 'metadata' in sub[2] and \
                     os.path.basename(sub[0]) == 'dom_md' and \
-                    self.is_iso_domain(os.path.join(sub[0], 'metadata')):
+                    self._is_iso_domain(os.path.join(sub[0], 'metadata')):
                 return os.path.join(
                     os.path.dirname(sub[0]),
                     'images',
