0206d0631ea1168a3762131674ed5eed0195c3d2
Tomáš Golembiovský <tgolembi@redhat.com>
2019-03-12 21:42:27 +0100

    DO NOT MERGE
    temporary hack to force automation into build

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 RPM_VERSION="1.9.2"
-if git describe --exact-match --tags --match "v[0-9]*" > /dev/null 2>&1 ; then
+#if git describe --exact-match --tags --match "v[0-9]*" > /dev/null 2>&1 ; then
     RPM_RELEASE="1"
-else
-    GIT="$(
-        git describe --always --tags --dirty=.dr |
-        sed -r 's/^/git/; s/^[^-]*-//; s/-g/.git/; s/-/_/g'
-    )"
-    RPM_RELEASE="0.$GIT.$(date -u +%Y%m%d%H%M%S)"
-fi
+#else
+#    GIT="$(
+#        git describe --always --tags --dirty=.dr |
+#        sed -r 's/^/git/; s/^[^-]*-//; s/-g/.git/; s/-/_/g'
+#    )"
+#    RPM_RELEASE="0.$GIT.$(date -u +%Y%m%d%H%M%S)"
+#fi
 
 ROLE_NAME="oVirt.v2v-conversion-host"
 PACKAGE_NAME="ovirt-ansible-v2v-conversion-host"
